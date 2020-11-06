ad0eecaf4e283e62a0deb6522233297600fbb4c7
ACRT <49489769+ACRTTool@users.noreply.github.com>
2020-11-06 10:22:18 -0500

    Add files via upload

--- JS/acrt_Web_Report.js
+++ JS/acrt_Web_Report.js
@@ -333,7 +333,7 @@ document.getElementById("dsblGrpBtn").click();
       }
       $scope.impactedGroup = [];
        document.getElementById("msg1").innerHTML = "<strong>"+$scope.evalMethod +" Version "+$scope.evalMethodVrsn +"</strong> file load completed.<br>";		  
-        alert('To save printer friendly HTML file, select the keyboard shortcut (Alt+s) or Save button located at the bottom of the page.');
+        alert('To save printer friendly HTML file, select the keyboard shortcut (Alt+S) or Save button located at the bottom of the page.');
 	  
 	  //$scope.validData=true;
       if($scope.jsonData[0].SuccessCriteria == undefined){
